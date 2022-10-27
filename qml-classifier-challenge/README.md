29689b4e377c3c23f61c048b52ec41dac5707461
Ryan <ryanjh88@gmail.com>
2022-10-27 17:28:21 -0500

    qml updates

---
+++ qml-classifier-challenge/README.md
@@ -0,0 +1,34 @@
+# QML Challenge
+
+## Variational quantum classifier
+
+In this challenge, you will use [Pennylane](https://github.com/PennyLaneAI/pennylane) and [Amazon Braket](https://github.com/aws/amazon-braket-examples) to implement a variational quantum classifier (VQC) to take on a supervised learning problem based on the work of [Havlíček et al. (2018)](https://arxiv.org/pdf/1804.11326.pdf). Implement each of the four TODOs in [`vqc_challenge.ipynb`](vqc_challenge.ipynb) to construct and train a QML model to solve the binary classification task. You can use the [`gen_binary.py`](gen_binary.py) script to generate a new train/test dataset, and further test your model.
+
+A central feature of Amazon Braket is that its remote simulator can execute multiple circuits in parallel. This capability can be harnessed in PennyLane during circuit training, which requires lots of variations of a circuit to be executed. Hence, the PennyLane-Braket plugin provides a method for scalable optimization of large circuits with many parameters. After validating your implementation and successfully training using a local simulator, enable [qBraid quantum jobs](https://qbraid-qbraid.readthedocs-hosted.com/en/latest/cli/jobs.html) and test your binary classifier on remote quantum hardware using the [Amazon Braket Pennylane plugin](https://github.com/aws/amazon-braket-pennylane-plugin-python). See the resources linked below for more.
+
+### Submissions
+
+- To submit your hack, create a pull request from your team's named branch
+- Submission PR title format: `[VQC-CHALLENGE] [team-name] [date]`
+- Submissions will be evaluated on a *rolling basis*
+- Each team is allowed *one submission per day*
+
+### Leaderboard
+
+A leaderboard will be kept on https://account.qbraid.com/haqs that ranks teams in order of their submitted VQC model accuracy. The leaderboard will be updated every 24 hours with all the submissions from that day. All teams that make a submission, no matter their model's performance, will appear on the leaderboard and therefore be eligable for prizes.
+
+*Important*:  Your final model must be tested with on a remote quantum device using the Amazon Braket Pennylane plugin. Submissions that do not interface with [Amazon Braket supported devices](https://docs.aws.amazon.com/braket/latest/developerguide/braket-devices.html) will not be eligible to be ranked on the leaderboard.
+
+### Resources:
+
+- [Pennylane Key Concepts](https://pennylane.ai/qml/glossary.html)
+- [PennyLane-Braket Plugin](https://amazon-braket-pennylane-plugin-python.readthedocs.io/en/latest/)
+- [Computing gradients in parallel with Amazon Braket](https://pennylane.ai/qml/demos/braket-parallel-gradients.html)
+- [Use PennyLane with Amazon Braket](https://docs.aws.amazon.com/braket/latest/developerguide/hybrid.html)
+- [Amazon Braket examples: hybrid quantum algorithms](https://github.com/aws/amazon-braket-examples/tree/main/examples/hybrid_quantum_algorithms)
+- [qBraid demo notebooks: quantum jobs](https://github.com/qBraid/qbraid-lab-demo/blob/main/qbraid_quantum_jobs.ipynb)
+- [qBraid CLI: quantum jobs commands](https://qbraid-qbraid.readthedocs-hosted.com/en/latest/cli/jobs.html)
+
+## Quantum kernel estimator
+
+If your team completes the VQC model with 100% test accuracy, you are eligible to move on to the quantum kernel estimator challenge, which will be used as a tie-breaker for those sharing 1st position on the leaderboard. Follow the instructions in [`kernel_estimator.ipynb`](kernel_estimator.ipynb), and submit your solution using the PR title format: `[KERNEL-ESTIMATOR] [team-name] [date]`. Submissions to this challenge will also be added to the leaderboard. If a second tie-breaker is needed between top submissions, the judges will choose the winner based on overall impression of the solution, e.g. is the code well written and easy to follow, is the implementation elegant or brute force, what plots other visualizations are included, etc.
---
+++ qml-classifier-challenge/gen_binary.py
@@ -0,0 +1,134 @@
+import os
+import csv
+import json
+import pennylane as qml
+import pennylane.numpy as np
+from scipy.stats import unitary_group
+
+
+def encode_data(x):
+    """Non-linear encoding (transformation) of one input data vector
+
+    Args:
+        x : shape (2,) tensor containing one input data vector
+
+    Returns:
+        triple of data encoded coefficients phi_1, phi_2, phi_{1,2}
+    """
+
+    return x[0], x[1], (np.pi - x[0]) * (np.pi - x[1])
+
+
+def feature_map(x, meas=False):
+    """Short depth feature map with entanglement
+
+    Args:
+        x : shape (3,) tensor containing one encoded data vector
+        meas : boolean where `True` prompts a probabilistic measurement at the end of the circuit
+
+    Returns:
+        (iff `meas = True`) shape (2,) tensor containing probability of each computational basis state
+    """
+
+    # apply series of Hadamards and diagonal phase gate compoenents
+    for _ in range(2):
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.RZ(-2 * x[0], wires=0)
+        qml.RZ(-2 * x[1], wires=1)
+        qml.CNOT(wires=[0, 1])
+        qml.RZ(-2 * x[2], wires=1)
+        qml.CNOT(wires=[0, 1])
+
+    if meas:
+        return qml.probs(wires=[0, 1])
+
+
+def gen_label(x, f, V, delta):
+    """Generate labels for data vectors
+
+    Args:
+        x : shape (3,) tensor containing one encoded data vector
+        f : shape (4,4) tensor representing the chosen parity function
+        V : shape (4,4) tensor representing a random unitary in SU(4)
+        delta : magnitude of data seperation
+
+    Returns:
+        one of {-1, 0, +1} according to expectation value of constructed observable w.r.t. feature mapped state
+    """
+
+    obs = np.conj(V).T @ f @ V  # constructed observable
+
+    dev = qml.device("default.qubit", wires=2)
+    fm_circuit = qml.QNode(feature_map, dev)
+    fm_circuit(encode_data(x), meas=True)
+    fm_state = (dev._state).flatten()  #  feature mapped state
+
+    output = np.real(np.conj(fm_state).T @ obs @ fm_state)  # expectation value
+
+    if output >= delta:
+        return 1
+    elif output <= -1 * delta:
+        return -1
+    else:
+        return 0
+
+
+def gen_data(n_points, **params):
+    """Generate data vectors and associated labels
+
+    Args:
+        n_points : number of data points
+        f : shape (4,4) tensor representing the chosen parity function
+        V : shape (4,4) tensor representing a random unitary in SU(4)
+        delta : magnitude of data seperation
+
+    Returns:
+        tuple of tensors containing data vectors Xs of shape (n_points,2) and associated labels Ys of shape (n_points,)
+    """
+    np.random.seed(42)
+
+    nonzero = []
+
+    Xs = np.zeros((n_points, 2))
+    Ys = np.zeros(n_points)
+
+    Xs = np.random.uniform(
+        0, 2 * np.pi, size=(n_points, 2)
+    )  # uniform distribution between [0, 2*pi)
+    Ys = np.zeros(n_points)
+
+    for i, x in enumerate(Xs):
+        Ys[i] = gen_label(x, **params)  # assign labels
+        if Ys[i] in [1.0, -1.0]:
+            nonzero.append(i)
+
+    return Xs[nonzero], Ys[nonzero]
+
+
+if __name__ == "__main__":
+
+    SAVE_PATH = "data/"
+
+    Z = np.array([[1, 0], [0, -1]])  # Pauli-Z operator
+    f = np.array(np.kron(Z, Z), requires_grad=False)  # Parity function
+    V = unitary_group.rvs(4)  # random 4x4 unitary
+    delta = 0.3  # gap between data vectors (larger = easier to seperate)
+    params = {"f": f, "V": V, "delta": delta}
+    n_points = 10000
+
+    Xs, Ys = gen_data(n_points, **params)
+
+    n_nonzero = len(Xs)
+
+    os.makedirs(SAVE_PATH, exist_ok=True)
+
+    with open(SAVE_PATH + "binary_data.csv", mode="w") as file:
+        csvwriter = csv.writer(file)
+
+        for i in range(n_nonzero):
+            csvwriter.writerow([Xs[i][0].item(), Xs[i][1].item(), Ys[i].item()])
+
+    with open(SAVE_PATH + "params.json", mode="w") as file:
+        save_params = {"f": str(f), "V": str(V), "delta": delta, "n_points": n_nonzero}
+        json.dump(save_params, file)
